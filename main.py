b627a63f03b40cb521a6ae6b03876853396ca92b
Jack Zimmerman <>
2021-05-17 02:16:03 +0000

    Added comments; Added valid coinbase transaction; Added Genesis Block generation; Added full block code; Changed values to be hex; Added fees;

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import hashlib
 from merklelib import MerkleTree
 
 import time
-import copy
 import json
 import ecdsa
 import codecs
@@ -13,7 +12,10 @@ import zlib, base64
 
 password = input("password for wallet: ")
 
-block_reward = 50000000
+block_reward = 50000000000
+
+##############################################################################
+#Hash functions for varying usage
 
 def stringHash(string):
     return hashlib.sha256(str(string).encode('ISO-8859-1')).hexdigest()
@@ -24,47 +26,71 @@ def rawHash(string):
 def addHash(string1, string2):
     return stringHash(rawHash(string1) + rawHash(string2))    
 
-def addHashRaw(string1, string2):
-    return rawHash(rawHash(string1) + rawHash(string2))
-
+#generates difficulty target based off of hex target
 def arbDifficulty(hashDemand):
     highest = bin((2**256)-1)
     hashDemand = int(hashDemand, 16)
     
     return bin(round((int(highest, 2))/hashDemand))
 
+
+##############################################################################
+
+#block/transaction verification:
+
 def verifyBlock(block):
     global block_reward
-
-    if (addHash(rawHash(block["header"]), block["nonce"]) != block["proof"]):
-        return False
     
+    try:
+        block = block.__dict__
+    except:
+        pass
+
+    for transaction in block["transactions"]:
+        if transaction["sender"] == "0000000000000000000000000000000000000000000000000000000000000000":
+            if transaction["txamount"] != block_reward:                
+                return False
+            elif ((transaction["outputs"][0][1]) + (transaction["outputs"][1][1])) != block_reward:
+                return False
+
+    if (addHash(rawHash(block["header"]), int(block["nonce"], 16)) != block["proof"]):
+        return False
 
     return True
 
-def verifyTransactions(blockOld):
-    block = copy.deepcopy(blockOld)
-
-    block.transactions = [(x.__dict__) for x in block.transactions]
-
-    block = block.__dict__
-
-    for x in block["transactions"]:
-        if (verifyTransaction(x) == False):
-            del x
-        
-        if x["sender"] == "COINBASE":
-            if x["txamount"] != block_reward:
-                del x
-            elif ((x["outputs"][0][1]) + (x["outputs"][1][1])) != block_reward:
-                del x
         
 
-def verifyTransaction(transaction):
+def verifyTransaction(blockchain, transaction):
     try:
-        ecdsa.VerifyingKey.from_string(bytes.fromhex(transaction["sender"]),curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256).verify(bytes(transaction["signed"], "ISO-8859-1"), bytes(transaction["txhash"] + transaction["nonce"], "ISO-8859-1"))
+        transaction = transaction.__dict__
+    except:
+        pass 
+    
+
+    #generating information on sender:
+    userBal = generateBalance(blockchain, transaction["sender"])
+    minerFee = len(str(json.dumps(transaction))) * 100
+
+    
+    try:
+        ecdsa.VerifyingKey.from_string(bytes.fromhex(transaction["sender"]),curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256).verify(bytes.fromhex(transaction["signed"]),bytes(transaction["txhash"] + transaction["nonce"], "ISO-8859-1"))
     except:
         return False
+        
+    if transaction["txamount"] > userBal:
+        return False
+    else:
+        total = 0
+        for x in transaction["outputs"]:
+            total += x[1]
+            
+        if total > userBal: 
+            return False 
+
+        if total < minerFee:
+            return False
+
+    return True
 
     
 def nodeVerifyTransaction(transaction):
@@ -72,22 +98,43 @@ def nodeVerifyTransaction(transaction):
         transaction.verifications += 1
 
 
+
+##############################################################################
+#account information grabbing
+def generateBalance(blockchain, account):
+    total = 0
+    for x in blockchain.chainDict:
+        for y in x["transactions"]:
+            for z in y["outputs"]:
+                if z[0] == account:
+                    total += z[1]
+
+            if y["sender"] == account:
+                total -= y["txamount"]
+
+    return total
+
             
-    
+##############################################################################
+#blockchain element classes
 
 
 class blockChain:
     def __init__(self):
         self.blockchain = []
         self.chainDict = []
-        self.size = len(self.chainDict)  
+        self.size = len(self.chainDict)
         self.dataSize = len(json.dumps(self.chainDict)) 
 
     def createGenesis(self, miner):
+        self.blockchain = []
+        self.chainDict = []
         genesis = block(self)
-        genesis.difficulty = "{:012x}".format(2000)
+        genesis.difficulty = "{:016x}".format(200000)
         genesis.complete(time.time())
         mine(genesis, miner, self)
+        self.blockchain.append(genesis)
+        self.chainDict.append(genesis.__dict__)
 
     def addBlock(self, block):
         self.blockchain.append(block)
@@ -96,8 +143,8 @@ class blockChain:
 
     def writeToFile(self):
         try:
-            data = json.dumps(self.chainDict)
-            data =  base64.b64encode(zlib.compress(data.encode('ISO-8859-1'),9)) 
+            data = json.dumps(self.chainDict, indent=4, sort_keys=False)
+            data =  base64.b64encode(zlib.compress(data.encode('ISO-8859-1'),9))
             data = data.decode('ISO-8859-1')
 
             file = open("blockchain.dat", 'w')
@@ -124,12 +171,12 @@ class blockChain:
         file.close()
 
         file = open("blockchaindecompressed.dat", 'w')
-        file.write(json.dumps(data))
+        file.write(json.dumps(data, indent=4))
         file.close()
 
     def syncChain(self):
         self.chainDict = self.retrieveFromFile()
-        blockchain.size = len(self.chainDict)
+        self.size = len(self.chainDict)
             
 
 
@@ -138,13 +185,14 @@ class block:
         self.height = blockchain.size
 
         if (self.height != 0):
-            self.previousHash = blockchain.chainDict[-1]["proof"] 
+            self.previousBlock = blockchain.chainDict[-1]["proof"] 
+            self.difficulty = self.calculateDifficuty(blockchain)
         else:
-            self.previousHash = "0"
+            self.previousBlock = "0"
+            self.difficulty = "{:016x}".format(1000)
 
         self.version = 0.1
         self.transactions = []
-        self.difficulty = self.calculateDifficuty(blockchain)
         self.nonce = 0
 
     def calculateDifficuty(self, blockChain):
@@ -153,38 +201,55 @@ class block:
         if (self.height % 144 == 0):
             total_time = round(time.time(), 2) - chain[self.height - 144]["timeStamp"]
             
-            return "{:012x}".format(round(int(chain[self.height - 1]["difficulty"],16) * ((1440 * 60)/total_time)))
+            return "{:016x}".format(round(int(chain[self.height - 1]["difficulty"],16) * ((1440 * 60)/total_time)))
         else:
             return chain[self.height - 1]["difficulty"]
     
-    def addTransaction(self, transaction):
-        self.transactions.append(transaction)
+    def addTransaction(self, blockchain, transaction):
+        if verifyTransaction(blockchain, transaction) == True:
+            self.transactions.append(transaction)
 
     def complete(self, timestamp): 
         self.timeStamp = round(timestamp, 2)
-        self.header = stringHash(json.dumps([(x.__dict__) for x in self.transactions]) + str(self.timeStamp))
+        self.header = stringHash(json.dumps([(x.__dict__) for x in self.transactions]) + str(self.previousBlock))
 
-    def addFees(self, miner, fee):
-        for x in range(len(self.transactions)):
-            self.transactions[x].addFee(miner, fee)
 
-    def addBlockToChain(self, blockchain, nonce):
-        self.nonce = nonce
+    def addBlockToChain(self, blockchain, nonce, miner):
+        #adding fees to all transactions in block
+        size, fee = 0, 0
+        for y,x in enumerate(self.transactions):
+            try:
+                size = len(str(json.dumps(self.transactions[y])))
+            except: 
+                size = len(str(json.dumps(self.transactions[y].__dict__)))
+            
+            try:
+                self.transactions[y] = self.transactions[y].__dict__
+            except:
+                self.transactions[y] = self.transactions[y]
+            
+            fee = size * 100
+
+            self.transactions[y]["outputs"][0] = [self.transactions[y]["outputs"][0][0], self.transactions[y]["txamount"] - fee]
+            self.transactions[y]["outputs"].append([miner, fee])
+
+        #formatting for saving chain
+        self.miner = miner
+        self.nonce = hex(nonce)
         self.main_chain = True
         self.tx_num = len(self.transactions)
 
         #murkleTree = MerkleTree(self.transactions, stringHash)
         self.murkleString = (str(MerkleTree(self.transactions, stringHash))[12:-2])
-        
-        self.transactions = [(x.__dict__) for x in self.transactions]
 
-        self.size = len(self.__dict__) * 8
+        self.size = len(json.dumps(self.__dict__)) * 8
         blockchain.addBlock(self)
 
 class transaction:
     privKey = ""
     def __init__(self, sender, reciever, amount, timestamp, privkey, nonce):
         global privKey
+
         privKey = privkey
         self.timestamp = round(timestamp, 2)
         self.sender = sender
@@ -192,19 +257,14 @@ class transaction:
         self.txamount = amount
         self.txhash = stringHash(sender + reciever + str(self.timestamp))
         self.verifications = 0
-        self.nonce = nonce 
+        self.nonce = '{:x}'.format(nonce)
         self.sign()
         
         
     def sign(self):
         global privKey
-        self.signed = str(ecdsa.SigningKey.from_string(bytes.fromhex(privKey),curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256).sign(bytes(self.txhash + self.nonce, 'ISO-8859-1')), 'ISO-8859-1')
-
-    def addFee(self, miner, fee):
-        self.outputs[0] = [self.outputs[0][0], self.txamount - fee]
-        self.outputs.append([miner, fee])
+        self.signed = ecdsa.SigningKey.from_string(bytes.fromhex(privKey),curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256).sign(bytes(self.txhash + self.nonce, 'ISO-8859-1')).hex()
         
-    
 
 
 class wallet: 
@@ -221,7 +281,7 @@ class wallet:
         self.publicHex = public.to_string("compressed").hex()
         privateHexUsable = private.to_string().hex()
         self.privateHex = cryptocode.encrypt(private.to_string().hex(), password)
-        self.nonce = str(0)
+        self.nonce = 0
         self.created = round(time.time(), 2)
 
         file = open("wallet.dat", 'w')
@@ -229,13 +289,16 @@ class wallet:
         file.close()
 
         self.privateHex = privateHexUsable
-    
+
+    def simpleTransaction(self, password, reciever, txamount):
+        return transaction(self.publicHex, reciever, txamount, round(time.time(), 2), self.retrievePrivate(password), self.retrieveNonce())
+
     def retrieveNonce(self):
         file = open("wallet.dat", 'r')
         wallet = json.loads(file.read())
         file.close()
 
-        wallet["nonce"] = str(int(wallet["nonce"]) + 1)
+        wallet["nonce"] = wallet["nonce"] + 1
 
         file = open("wallet.dat", 'w')
         file.write(json.dumps(wallet))
@@ -243,12 +306,11 @@ class wallet:
 
         return wallet["nonce"]
 
-    def retrievePrivate(self):
-        global password
+    def retrievePrivate(self, password):
         file = open("wallet.dat", 'r')
         wallet = json.loads(file.read())
         file.close()
-
+        
         return cryptocode.decrypt(wallet["privateHex"], password)
         
     def retrievePublic(self):
@@ -270,11 +332,12 @@ class wallet:
 
 
 
+##############################################################################
 #non class functions
     
 
 def mine(block, miner, blockchain):
-    global difficulty 
+    global password 
     hash = rawHash(block.header)
     startTime = time.time()
 
@@ -296,10 +359,9 @@ def mine(block, miner, blockchain):
                 except:
                     print("infinite luck")
 
-                block.addTransaction(transaction("COINBASE", miner.publicHex, 50000000, round(time.time(), 2), miner.privateHex, miner.retrieveNonce()))
-                block.addFees(miner.publicHex, len(block.transactions) * 1000)
+                block.transactions.append(transaction(''.join([("0") for x in range(64)]), miner.publicHex, 50000000000, round(time.time(), 2), miner.retrievePrivate(password), miner.retrieveNonce()))
                 block.proof = calculated
-                block.addBlockToChain(blockchain, x)
+                block.addBlockToChain(blockchain, x, miner.publicHex)
                 return
         
         block.complete(block.timeStamp + 0.001)
@@ -309,23 +371,24 @@ def mine(block, miner, blockchain):
 
 
 wallet1 = wallet()
-wallet1.generateKeys(password)
+wallet1.publicHex = wallet1.retrievePublic()
 
-wallet2 = wallet()
-wallet2.generateKeys(password)
 
 blockchain = blockChain()
-blockchain.createGenesis(wallet1)
+blockchain.syncChain()
 
-for x in range(10):
+"""
+for x in range(87):
     block1 = block(blockchain)
-
-    block1.addTransaction(transaction(wallet2.publicHex, wallet1.publicHex, 50000000, round(time.time(), 2), wallet2.privateHex, wallet2.retrieveNonce()))
-
-    verifyTransactions(block1)
-
+        
     block1.complete(time.time())
     mine(block1, wallet1, blockchain)
+"""
+
+block1 = block(blockchain)
+block1.addTransaction(blockchain, wallet1.simpleTransaction(password, stringHash("lmao??????"), 550000000))
+block1.complete(time.time())
+mine(block1, wallet1, blockchain)
 
 blockchain.writeToFile()
 blockchain.decompressFile()
